1228da2cd5b9ef07dcbe69d1a1f0924b74433bfa
Rabah0986 <127623280+Rabah0986@users.noreply.github.com>
2025-03-19 07:50:42 +0100

    Update Arun1.js

--- Priyansh/commands/Arun1.js
+++ Priyansh/commands/Arun1.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "المطور",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Arun", 
+	credits: "المطور", 
 	description: "no prefix",
 	commandCategory: "No command marks needed",
 	usages: "...",
@@ -13,9 +13,9 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("owner") ||
-     react.includes("Arun") || 
-react.includes("arun")) {
+	if(react.includes("مطور") ||
+     react.includes("المطور") || 
+react.includes("الادمن")) {
 		var msg = {
 				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐀𝐫𝐮𝐧 𝐊𝐮𝐦𝐚𝐫✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
 				attachment: fs.createReadStream(__dirname + `https://j.top4top.io/p_33653vp3p0.jpg`)
